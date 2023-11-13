4f662c544fe85ea258ba9165a0a4f40d0915b825
Hugo.M <h.maestracci83@gmail.com>
2023-11-13 01:16:21 +0100

    feature: board enhancement for game logic

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,5 +1,12 @@
 #include "TicTacToe.h"
 
+#include <iostream>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+#include "../Client/src/core/ClientApp.h"
+
+
 #if defined(DEBUG) | defined(_DEBUG)
 #include <crtdbg.h>
 // Dynamically allocate memory with memory leak detection. (debug only)
@@ -8,35 +15,152 @@
 
 namespace TicTacToe
 {
-Board::Board(size_t width, size_t height) : width(width), height(height), size(width * height)
-{
-    m_board = new Piece[size];
-    Clear();
-}
+	Board::Board(size_t width, size_t height, int pieceSize) : width(width), height(height), size(width * height), pieceSize(pieceSize)
+	{
+		for (size_t i = 0; i < size; ++i)
+		{
+			m_board.push_back(new Piece());
+		}
+	}
 
-Board::~Board()
-{
-    delete[] m_board;
-    m_board = nullptr;
-}
+	Board::~Board()
+	{
+	    for (auto& piece : m_board)
+	    {
+		    RELEASE(piece);
+	    }
+	}
 
-bool Board::IsFull() const
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (m_board[i] == Piece::None)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+	bool Board::IsFull() const
+	{
+	    for (int i = 0; i < size; i++)
+	    {
+	        if (m_board[i]->GetPlayerID() == EMPTY_PIECE)
+	        {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 
-void Board::Clear()
-{
-    for (int i = 0; i < size; i++)
-    {
-        m_board[i] = Piece::None;
-    }
-}
+	void Board::Clear()
+	{
+	    for (int i = 0; i < size; i++)
+	    {
+			m_board[i]->Clear();
+	    }
+	}
+
+	bool Board::CheckForWin()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (m_board[i]->GetPlayerID() == EMPTY_PIECE)
+			{
+				continue;
+			}
+
+			// Check for horizontal win
+			if (i % width == 0)
+			{
+				for (int j = 0; j < width; j++)
+				{
+					if (m_board[i + j]->GetPlayerID() != m_board[i]->GetPlayerID())
+					{
+						break;
+					}
+					if (j == width - 1)
+					{
+						// We have a winner!
+						std::cout << "Player " << m_board[i]->GetPlayerID() << " has won!\n";
+						return true;
+					}
+				}
+			}
+
+			// Check for vertical win
+			if (i < width)
+			{
+				for (int j = 0; j < size; j += width)
+				{
+					if (m_board[i + j]->GetPlayerID() != m_board[i]->GetPlayerID())
+					{
+						break;
+					}
+					if (j == size - width)
+					{
+												// We have a winner!
+						std::cout << "Player " << m_board[i]->GetPlayerID() << " has won!\n";
+						return true;
+					}
+				}
+			}
+
+			// Check for top-left diagonal win
+			if (i == 0)
+			{
+				for (int j = 0; j < size; j += width + 1)
+				{
+					if (m_board[i + j]->GetPlayerID() != m_board[i]->GetPlayerID())
+					{
+						break;
+					}
+					if (j == size - 1)
+					{
+												// We have a winner!
+						std::cout << "Player " << m_board[i]->GetPlayerID() << " has won!\n";
+						return true;
+					}
+				}
+			}
+
+			// Check  for top-right diagonal win
+			if (i == width - 1)
+			{
+				for (int j = 0; j < size - 1; j += width - 1)
+				{
+					if (m_board[i + j]->GetPlayerID() != m_board[i]->GetPlayerID())
+					{
+						break;
+					}
+					if (i + j == size - width)
+					{
+						// We have a winner!
+						std::cout << "Player " << m_board[i]->GetPlayerID() << " has won!\n";
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	Piece::Piece()
+	{
+		m_Shape = new sf::RectangleShape(sf::Vector2f(100, 100));
+		m_Shape->setFillColor(sf::Color::Color(51, 56, 63));
+		m_Shape->setOutlineThickness(10);
+		m_Shape->setOutlineColor(sf::Color::Red);
+		m_PlayerID = EMPTY_PIECE;
+	}
+
+	Piece::~Piece()
+	{
+		Clear();
+	}
+
+	void Piece::SetPlayerPiece(const Player* player)
+	{
+		const sf::Vector2f currentPosition = GetPosition();
+
+		m_Shape = player->GetShape();
+		m_Shape->setPosition(currentPosition);
+		m_PlayerID = player->GetPlayerID();
+	}
+
+	void Piece::Clear()
+	{
+		NULLPTR(m_Shape)
+		m_PlayerID = EMPTY_PIECE;
+	}
+
 }
--- TicTacToe/TicTacToe.h
+++ TicTacToe/TicTacToe.h
@@ -1,63 +1,93 @@
+#pragma once
+#include <string>
+
+#include "Player.h"
+
+#include "../Client/src/core/CoreDefinitions.h"
+
+
 namespace TicTacToe
 {
-/// <summary>
-/// Represents a piece on the board.
-/// </summary>
-enum class Piece : char
-{
-    None = ' ',
-    X = 'X',
-    O = 'O',
-};
+	/// <summary>
+	/// Represents a piece on the board.
+	/// </summary>
+	class Piece
+	{
+	public:
+		Piece();
+		Piece(sf::Vector2f position);
+		~Piece();
 
-/// <summary>
-/// Represents a board of Tic-Tac-Toe.
-/// </summary>
-class Board
-{
-public:
-    /// <summary>
-    /// Creates a 3x3 board.
-    /// </summary>
-    Board() : Board(3, 3) {}
-    /// <summary>
-    /// Creates a board with the specified width and height.
-    /// </summary>
-    Board(size_t width, size_t height);
-    ~Board();
+		sf::Shape* GetShape() const { return m_Shape; }
+		int GetPlayerID() const { return m_PlayerID; }
+		sf::Vector2f GetPosition() const { return m_Shape->getPosition(); }
 
-    size_t GetWidth() const { return width; }
-    size_t GetHeight() const { return height; }
-    size_t GetSize() const { return size; }
+		void SetPlayerPiece(const Player* player);
+		void SetPosition(sf::Vector2f position) { m_Shape->setPosition(position); }
 
-    /// <summary>
-    /// Returns a reference to the piece at the specified row and column.
-    /// </summary>
-    Piece& operator()(size_t row, size_t col) { return m_board[row * width + col]; }
-    /// <summary>
-    /// Returns a const reference to the piece at the specified row and column.
-    /// </summary>
-    const Piece& operator()(size_t row, size_t col) const { return m_board[row * width + col]; }
-    /// <summary>
-    /// Returns a reference to the piece at the specified index.
-    /// </summary>
-    Piece& operator[](size_t index) { return m_board[index]; }
-    /// <summary>
-    /// Returns a const reference to the piece at the specified index.
-    /// </summary>
-    const Piece& operator[](size_t index) const { return m_board[index]; }
+		void Clear();
 
-    /// <summary>
-    /// Returns true if the board does not contain any Piece::None.
-    /// </summary>
-    bool IsFull() const;
-    /// <summary>
-    /// Sets all the pieces on the board to Piece::None.
-    /// </summary>
-    void Clear();
+	private:
 
-protected:
-    size_t width = 3, height = 3, size = 9;
-    Piece* m_board = nullptr;
-};
+		sf::Shape* m_Shape = nullptr;
+		int m_PlayerID = EMPTY_PIECE;
+
+	};
+
+	/// <summary>
+	/// Represents a board of Tic-Tac-Toe.
+	/// </summary>
+	class Board
+	{
+	public:
+	    /// <summary>
+	    /// Creates a 3x3 board.
+	    /// </summary>
+	    Board() : Board(3, 3, 100) {}
+	    /// <summary>
+	    /// Creates a board with the specified width and height.
+	    /// </summary>
+	    Board(size_t width, size_t height, int pieceSize);
+	    ~Board();
+
+	    size_t GetWidth() const { return width; }
+	    size_t GetHeight() const { return height; }
+	    size_t GetTotalSize() const { return size; }
+		size_t GetPieceSize() const { return pieceSize; }
+		Piece& GetPieceAt(size_t index) const { return *m_board[index]; }
+		Piece& GetPieceAt(size_t row, size_t col) const { return *m_board[row * width + col]; }
+
+
+	    /// <summary>
+	    /// Returns a reference to the piece at the specified row and column.
+	    /// </summary>
+	    Piece& operator()(size_t row, size_t col) { return *m_board[row * width + col]; }
+	    /// <summary>
+	    /// Returns a const reference to the piece at the specified row and column.
+	    /// </summary>
+	    const Piece& operator()(size_t row, size_t col) const { return *m_board[row * width + col]; }
+	    /// <summary>
+	    /// Returns a reference to the piece at the specified index.
+	    /// </summary>
+	    Piece& operator[](size_t index) { return *m_board[index]; }
+	    /// <summary>
+	    /// Returns a const reference to the piece at the specified index.
+	    /// </summary>
+	    const Piece& operator[](size_t index) const { return *m_board[index]; }
+
+	    /// <summary>
+	    /// Returns true if the board does not contain any Piece::None.
+	    /// </summary>
+	    bool IsFull() const;
+	    /// <summary>
+	    /// Sets all the pieces on the board to Piece::None.
+	    /// </summary>
+	    void Clear();
+	    bool CheckForWin();
+
+    protected:
+	    size_t width = 3, height = 3, size = 9;
+		int pieceSize = 100;
+	    std::vector<Piece*> m_board;
+	};
 }
