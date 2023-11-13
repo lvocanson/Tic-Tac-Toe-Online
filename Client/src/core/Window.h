a037aad976be7d7e4c4587ff380aa22e41d03890
Hugo.M <h.maestracci83@gmail.com>
2023-11-14 00:38:12 +0100

    chore: get window height and width

--- Client/src/core/Window.h
+++ Client/src/core/Window.h
@@ -69,6 +69,8 @@ public:
 #pragma region Other
 
     sf::Vector2f GetCenter() const { return sf::Vector2f(m_Window->getSize().x * 0.5f, m_Window->getSize().y *0.5f); }
+    int GetWidth() const { return m_Window->getSize().x; }
+    int GetHeight() const { return m_Window->getSize().y; }
 
 #pragma endregion
 
