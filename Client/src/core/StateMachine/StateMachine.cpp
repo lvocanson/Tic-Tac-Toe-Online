442f4bcd5bb7985b18cd330eb8f07594402a7411
Gotvna <nvogt@gaming.tech>
2023-11-14 11:39:56 +0100

    ft(StateMachine) : Update StateMachine

--- Client/src/core/StateMachine/StateMachine.cpp
+++ Client/src/core/StateMachine/StateMachine.cpp
@@ -8,12 +8,34 @@ StateMachine::StateMachine()
 void StateMachine::InitState(State* initState)
 {
 	m_CurrentState = initState;
+}
+
+void StateMachine::Start()
+{
+	if (!m_CurrentState) 
+	{
+		std::cout << "State machine isn't init" << std::endl;
+		return;
+	}
+
 	m_CurrentState->OnEnter();
 }
 
-void StateMachine::SetState(State& newState)
+void StateMachine::AddState(std::string stateName, State* newState)
 {
+	m_States.insert(std::pair<std::string, State*>(stateName, newState));
+}
+
+void StateMachine::SwitchState(std::string newState)
+{
+	State* state = m_States[newState];
+	if (!state)
+	{
+		std::cout << "State not found" << std::endl;
+		return;
+	}
+
 	m_CurrentState->OnExit();
-	m_CurrentState = &newState;
+	m_CurrentState = state;
 	m_CurrentState->OnEnter();
 }
--- Client/src/core/StateMachine/StateMachine.h
+++ Client/src/core/StateMachine/StateMachine.h
@@ -1,18 +1,31 @@
 #pragma once 
 #include "State.h"
+#include <map>
 
 class StateMachine
 {
 public:
 	StateMachine();
+	 
+	/// <summary>
+	/// Add the state ref to the dictionnary
+	/// </summary>
+	void AddState(std::string stateName, State* newState);
 
 	void InitState(State* initState);
+	void Start();
+
+	/// <summary>
+	/// Switch the current state to the state with the name newState
+	/// </summary>
+	void SwitchState(std::string newState);
 
 	inline State* GetCurrentState() const { return m_CurrentState; }
-	void SetState(State& newState);
 
 private:
 
 	State* m_CurrentState;
+
+	std::map <std::string, State*> m_States;
 
 };
