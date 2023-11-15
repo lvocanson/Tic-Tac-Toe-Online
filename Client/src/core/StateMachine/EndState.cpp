fa9d3183dabdc1fc39c77617881fc497420d881c
Gotvna <nvogt@gaming.tech>
2023-11-15 15:18:54 +0100

    ft(StateMachine)  : prepare merge

--- Client/src/core/StateMachine/EndState.cpp
+++ Client/src/core/StateMachine/EndState.cpp
@@ -2,26 +2,26 @@
 
 void EndState::OnEnter()
 {
-    sf::Text* scoreText = new sf::Text("Score : " + std::to_string(m_Score), font, 20);
-    scoreText->setPosition(200, 200);
+    //sf::Text* scoreText = new sf::Text("Score : " + std::to_string(m_Score), font, 20);
+    //scoreText->setPosition(200, 200);
 
-    sf::RectangleShape* replayButton = new sf::RectangleShape(sf::Vector2f(150, 50));
-    replayButton->setPosition(100, 400);
-    replayButton->setFillColor(sf::Color::Green);
-    sf::Text* replayButtonText = new sf::Text("Rejouer", font, 20);
-    replayButtonText->setPosition(replayButton->getPosition().x + 20, replayButton->getPosition().y + 10);
+    //sf::RectangleShape* replayButton = new sf::RectangleShape(sf::Vector2f(150, 50));
+    //replayButton->setPosition(100, 400);
+    //replayButton->setFillColor(sf::Color::Green);
+    //sf::Text* replayButtonText = new sf::Text("Rejouer", font, 20);
+    //replayButtonText->setPosition(replayButton->getPosition().x + 20, replayButton->getPosition().y + 10);
 
-    sf::RectangleShape* menuButton = new sf::RectangleShape(sf::Vector2f(150, 50));
-    menuButton->setPosition(300, 400);
-    menuButton->setFillColor(sf::Color::Blue);
-    sf::Text* menuButtonText = new sf::Text("Menu principal", font, 20);
-    menuButtonText->setPosition(menuButton->getPosition().x + 10, menuButton->getPosition().y + 10);
+    //sf::RectangleShape* menuButton = new sf::RectangleShape(sf::Vector2f(150, 50));
+    //menuButton->setPosition(300, 400);
+    //menuButton->setFillColor(sf::Color::Blue);
+    //sf::Text* menuButtonText = new sf::Text("Menu principal", font, 20);
+    //menuButtonText->setPosition(menuButton->getPosition().x + 10, menuButton->getPosition().y + 10);
 
-    m_Window->RegisterDrawable(scoreText);
-    m_Window->RegisterDrawable(replayButton);
-    m_Window->RegisterDrawable(replayButtonText);
-    m_Window->RegisterDrawable(menuButton);
-    m_Window->RegisterDrawable(menuButtonText);
+    //m_Window->RegisterDrawable(scoreText);
+    //m_Window->RegisterDrawable(replayButton);
+    //m_Window->RegisterDrawable(replayButtonText);
+    //m_Window->RegisterDrawable(menuButton);
+    //m_Window->RegisterDrawable(menuButtonText);
 }
 
 void EndState::OnUpdate(float dt)
