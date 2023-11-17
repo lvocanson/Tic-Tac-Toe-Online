33db2f2415eae10478b1c568f949587eb197da85
Hugo Maestracci <h.maestracci83@gmail.com>
2023-11-17 13:12:53 +0100

    chore: state machine file reoagnization

---
+++ Client/src/core/StateMachine/GameStates/MenuState.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "StateMachine.h"
+#include "State.h"
+#include "src/core/Window.h"
+#include "src/core/Components/ButtonComponent.h"
+
+
+class MenuState : public State
+{
+public:
+	void OnEnter() override;
+	void OnUpdate(float dt) override;
+	void OnExit() override;
+
+	MenuState(StateMachine* stateMachine, Window* window);
+	MenuState(const MenuState& other) = delete;
+	MenuState& operator=(const MenuState& other) = delete;
+	~MenuState();
+
+private:
+	Window* m_Window;
+
+	ButtonComponent* m_PlayButton;
+	ButtonComponent* m_HistoryButton;
+	ButtonComponent* m_QuitButton;
+};
